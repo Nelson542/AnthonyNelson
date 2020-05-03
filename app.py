097721d3bb638caf42802887e40dd3a5c22e7b72
Nelson542 <37173830+Nelson542@users.noreply.github.com>
2020-05-03 21:59:21 +0530

    first_dict

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, render_template, request,json
+app = Flask(__name__)
+
+
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "static/data", "data.json")
+data = json.load(open(json_url))
+
+
+
+@app.route('/')
+def form():
+   return render_template('form.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        word = request.form['word']
+        if word in data:
+            output = data[word]
+            return render_template('result.html',  **locals() )
+        elif word.title() in data:
+            output =  data[word.title()]
+            return render_template('result.html',  **locals())
+        else:
+            No = "Word doesn't exist"
+            return render_template('result.html', No=No)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
